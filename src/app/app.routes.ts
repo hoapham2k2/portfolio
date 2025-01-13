84c510f1f0cdf609ff70bb717c128eeeda358a47
hoapham2k2 <20520995@gm.uit.edu.vn>
2025-01-13 13:30:14 +0700

    Update route
    Co-authored-by: hoapham2k2 <20520995@gm.uit.edu.vn>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
+];
